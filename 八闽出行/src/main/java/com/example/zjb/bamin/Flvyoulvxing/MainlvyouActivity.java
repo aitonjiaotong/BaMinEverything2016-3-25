6480c442376a83ed8015af2a295d6bad4b926511
Administrator <zjb@aiton.com.cn>
2016-03-16 18:16:48 +0800

    2016年3月16日17:32:49

--- 八闽出行/src/main/java/com/example/zjb/bamin/Flvyoulvxing/MainlvyouActivity.java
+++ 八闽出行/src/main/java/com/example/zjb/bamin/Flvyoulvxing/MainlvyouActivity.java
@@ -1,63 +1,95 @@
 package com.example.zjb.bamin.Flvyoulvxing;
 
-import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.example.zjb.bamin.R;
 
-public class MainlvyouActivity extends AppCompatActivity {
+public class MainlvyouActivity extends AppCompatActivity implements View.OnClickListener
+{
+
+    private String mWebViewUrl = "http://u.ctrip.com/HotelCSPSit/PhoenixPage/index.html?allianceid=310999&Sid=790059";
+    private WebView mWv_journey;
+    private ImageView mIv_journey_back;
+    private LinearLayout mLl_loading;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_lvyou);
+
+        findViewID();
+        initUI();
+        setListener();
     }
-    public void chujingyou(View view){
-        Intent intent= new Intent();
-        intent.setClass(MainlvyouActivity.this, ChuJingYouActivity.class);
-        startActivity(intent);
+
+    private void findViewID()
+    {
+        mIv_journey_back = (ImageView) findViewById(R.id.iv_journey_back);
+        mWv_journey = (WebView) findViewById(R.id.wv_journey);
+        mLl_loading = (LinearLayout) findViewById(R.id.ll_loading);
     }
-    public void guoneiyou(View view){
-        Intent intent= new Intent();
-        intent.setClass(MainlvyouActivity.this, GuoNeiYouActivity.class);
-        startActivity(intent);
+
+    private void initUI()
+    {
+        initWebView();
     }
-    public void zhoubianyou(View view){
-        Intent intent= new Intent();
-        intent.setClass(MainlvyouActivity.this, ZhouBianYouActivity.class);
-        startActivity(intent);
+
+    private void initWebView()
+    {
+        WebSettings settings = mWv_journey.getSettings();
+        settings.setJavaScriptEnabled(true);
+        // 设置可以支持缩放
+        settings.setSupportZoom(true);
+        //扩大比例的缩放
+        settings.setUseWideViewPort(true);
+        //自适应屏幕
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        settings.setLoadWithOverviewMode(true);
+
+        mWv_journey.setWebViewClient(new MyWebViewClient());
+        mWv_journey.loadUrl(mWebViewUrl);
     }
-    public void dingdan(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
+
+    private void setListener()
+    {
+        mIv_journey_back.setOnClickListener(this);
     }
-    public void gerenzhongxin(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
+
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.iv_journey_back:
+                finish();
+                break;
+        }
     }
-    public void pingtuanyou(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void sirendingzhi(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void zijiayou(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void jingdinaqupiao(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void jiudian(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void huochepiao(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void jipiao(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
-    }
-    public void qianzheng(View view){
-        Toast.makeText(MainlvyouActivity.this, "功能未完善", Toast.LENGTH_SHORT).show();
+
+    class MyWebViewClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon)
+        {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url)
+        {
+            super.onPageFinished(view, url);
+            mLl_loading.setVisibility(View.GONE);
+            mWv_journey.setVisibility(View.VISIBLE);
+        }
     }
 }
