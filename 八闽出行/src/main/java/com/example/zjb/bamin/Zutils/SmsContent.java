02810138966d44caf18953691f371b0bab3d1bfd
zjb <zjb@aiton.com.cn>
2016-03-18 18:17:31 +0800

    2016年3月18日16:42:11

--- 八闽出行/src/main/java/com/example/zjb/bamin/Zutils/SmsContent.java
+++ 八闽出行/src/main/java/com/example/zjb/bamin/Zutils/SmsContent.java
@@ -11,7 +11,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SmsContent extends ContentObserver {
- 
+
  public static final String SMS_URI_INBOX = "content://sms/inbox";
  private Activity activity = null;
  private String smsContent = "";
@@ -27,7 +27,7 @@ public class SmsContent extends ContentObserver {
   Cursor cursor = null;// 光标
   // 读取收件箱中指定号码的短信
   cursor = activity.managedQuery(Uri.parse(SMS_URI_INBOX), new String[] { "_id", "address", "body", "read" }, "address=? and read=?",
-                                 new String[] { "10657120610111", "0" }, "date desc");
+                                 new String[] { "1069003200004", "0" }, "date desc");
   if (cursor != null) {// 如果短信为未读模式
    cursor.moveToFirst();
    if (cursor.moveToFirst()) {
